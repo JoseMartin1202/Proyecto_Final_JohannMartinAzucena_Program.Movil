f5cbe98615cca011e20a0c235e0210e11c22efa9
Martin <josemartingarciamartinez237@gmail.com>
2023-09-29 12:15:22 -0600

    Agregar strings necesarios, así como agregar los modelos para las tareas o notas, de igual forma añadir un datasource de estas. También se agregan otras imágenes vectoriales.

---
+++ ProyectoFinal_JMA/app/src/main/java/com/example/proyectofinal_jma/data/DataSourceNotesOrHomework.kt
@@ -0,0 +1,15 @@
+package com.example.proyectofinal_jma.data
+
+import com.example.proyectofinal_jma.R
+import com.example.proyectofinal_jma.model.HomeworkOrNote
+
+object DataSourceNotesOrHomework {
+    val cardsHomeworkOrNote= listOf(
+        HomeworkOrNote(R.drawable.image,R.string.title,R.string.homeworkDescription,R.string.date,R.drawable.star,R.drawable.bell_outlined),
+        HomeworkOrNote(R.drawable.image,R.string.title,R.string.noteDescription,R.string.date,R.drawable.star,R.drawable.bell_outlined),
+        HomeworkOrNote(R.drawable.image,R.string.title,R.string.homeworkDescription,R.string.date,R.drawable.star,R.drawable.bell_outlined),
+        HomeworkOrNote(R.drawable.image,R.string.title,R.string.noteDescription,R.string.date,R.drawable.star,R.drawable.bell_outlined),
+        HomeworkOrNote(R.drawable.image,R.string.title,R.string.homeworkDescription,R.string.date,R.drawable.star,R.drawable.bell_outlined),
+        HomeworkOrNote(R.drawable.image,R.string.title,R.string.noteDescription,R.string.date,R.drawable.star,R.drawable.bell_outlined)
+    )
+}
---
+++ ProyectoFinal_JMA/app/src/main/java/com/example/proyectofinal_jma/model/LanguageOption.kt
@@ -0,0 +1,9 @@
+package com.example.proyectofinal_jma.model
+
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
+
+data class LanguageOption(
+    @StringRes val languageOpt:Int,
+    @DrawableRes val check:Int
+)

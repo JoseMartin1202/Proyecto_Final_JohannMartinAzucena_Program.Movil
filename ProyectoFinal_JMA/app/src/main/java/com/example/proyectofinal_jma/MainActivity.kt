1b56a08d3b05cb6828925f39e0b070ca3beae394
Martin <josemartingarciamartinez237@gmail.com>
2023-09-30 18:32:42 -0600

    Añadir topbar (barra de busqueda e ícono de filtro)

--- ProyectoFinal_JMA/app/src/main/java/com/example/proyectofinal_jma/MainActivity.kt
+++ ProyectoFinal_JMA/app/src/main/java/com/example/proyectofinal_jma/MainActivity.kt
@@ -1,39 +1,51 @@
 package com.example.proyectofinal_jma
 
+import android.graphics.drawable.shapes.Shape
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.sizeIn
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Card
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.proyectofinal_jma.data.DataSourceNotesOrHomework.cardsHomeworks
 import com.example.proyectofinal_jma.data.DataSourceNotesOrHomework.cardsNotes
 import com.example.proyectofinal_jma.model.Homework
 import com.example.proyectofinal_jma.model.Note
 import com.example.proyectofinal_jma.ui.theme.ProyectoFinal_JMATheme
+import com.example.proyectofinal_jma.ui.theme.Shapes
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -44,7 +56,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    ListElements()
+
                 }
             }
         }
@@ -187,10 +199,11 @@ fun HomeworkPreview() {
     }
 }
 
-@Preview(showBackground = true)
 @Composable
-fun ListElements() {
-    LazyColumn(modifier = Modifier.padding(dimensionResource(id = R.dimen.padding_8))){
+fun ListElements(
+    contentPadding: PaddingValues = PaddingValues(0.dp)
+) {
+    LazyColumn(contentPadding=contentPadding, modifier = Modifier.padding(dimensionResource(id = R.dimen.padding_4))){
         items(cardsHomeworks){
             HomeworkCard(homework= it)
         }
@@ -200,3 +213,49 @@ fun ListElements() {
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
+@Preview(showBackground = true)
+@Composable
+fun App(modifier: Modifier= Modifier) {
+    Scaffold(
+        modifier = Modifier
+            .fillMaxSize(),
+        topBar = {
+            Row(
+                verticalAlignment = Alignment.CenterVertically,
+                modifier = modifier.padding(end = 8.dp)
+            ){
+                Box (modifier=modifier.wrapContentHeight()){
+                    Image(
+                        painter = painterResource(id = R.drawable.filter),
+                        contentDescription = null,
+                        modifier = modifier
+                            .height(45.dp)
+                            .aspectRatio(1f)
+                            .padding(start = dimensionResource(id = R.dimen.padding_8))
+                    )
+                }
+                Spacer(Modifier.width(dimensionResource(id = R.dimen.padding_8)))
+                TextField(
+                    value = stringResource(id = R.string.buscar),
+                    onValueChange = {},
+                    modifier = Modifier
+                        .padding(top = 4.dp)
+                        .height(50.dp)
+                        .fillMaxWidth(),
+                    trailingIcon = { Icon(
+                        painter = painterResource(id = R.drawable.search) ,
+                        contentDescription = null)},
+                    shape = Shapes.large,
+                    colors = TextFieldDefaults.textFieldColors(
+                        focusedIndicatorColor = Color.Transparent,
+                        unfocusedIndicatorColor = Color.Transparent
+                    ),
+                    textStyle = MaterialTheme.typography.bodyMedium)
+
+            }
+        }
+    ) {
+        ListElements(contentPadding = it)
+    }
+}
